de18493b01dd3d6f70830efb857c0eea823b2aea
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-09-13 11:32:43 +0200

    Turn example into module

--- examples/country.pl
+++ examples/country.pl
@@ -1,6 +1,18 @@
+:- module(country,
+	  [ get_isd/2			% +CountryName, -ISD
+	  ]).
 :- use_module(library(wsdl)).
 :- use_module(library(xpath)).
 :- use_module(library(soap)).
+
+/** <module>
+
+Simple demo that  uses   http://www.webservicex.net/country.asmx  to get
+basic facts about countries such as their dialing code, currency etc.
+
+The WSDL description is in =|country.wsdl|=.   The latest version can be
+downloaded from http://www.webservicex.net/country.asmx?wsdl
+*/
 
 :- debug(soap).
 
