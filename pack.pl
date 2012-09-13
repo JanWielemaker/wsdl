75f8705cd63900590bfd402a327c08f4c14f5902
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-09-13 14:12:57 +0200

    Use tgz download

--- pack.pl
+++ pack.pl
@@ -3,4 +3,4 @@ version('0.1').
 title('Web Service Description Language (WSDL) and SOAP').
 keywords([wsdl, soap]).
 author('Jan Wielemaker', 'jan@swi-prolog.org').
-download('http://www.swi-prolog.org/home/jan/git/wsdl.git').
+download('http://www.swi-prolog.org/download/pack/wsdl-*.tgz').
